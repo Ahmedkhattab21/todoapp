495a00740d26e6381dbef622bbfc47d2fb111ccc
Ahmedkhattab14 <khattabahmed723@gmail.com>
2022-06-01 03:57:23 +0200

    ed

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -28,7 +28,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TaskController _taskController = Get.put(TaskController());
-  //kllk
   late NotifyHelper notifyHelper;
   @override
   void initState() {
